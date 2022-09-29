a5d47cf5bbbee2123894c04963b700ec508d976f
muneeb29 <98808077+muneeb29@users.noreply.github.com>
2022-09-29 14:20:34 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Home from "./views/Home.js";
+import Layout from "./layout/Layout.js";
+import Footer from "./layout/Footer.js"
+import "./index.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Layout>
+      <Footer/>
     </div>
   );
 }
---
+++ src/views/Home.js
@@ -0,0 +1,12 @@
+function Home(){
+
+    return(
+        <div>
+            <div className="container bg-light">
+            <h1>Home</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
